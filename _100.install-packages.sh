efb7ff44577f8000fb77456870e7fab1f0d58f0c
arturolegovich <82318393+arturolegovich@users.noreply.github.com>
2021-08-18 00:31:19 +0400

    Update _100.install-packages.sh

--- _100.install-packages.sh
+++ _100.install-packages.sh
@@ -8,7 +8,7 @@ yum -y update
 #reboot
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 yum -y install https://www.elrepo.org/elrepo-release-7.0-3.el7.elrepo.noarch.rpm
-yum --enablerepo=elrepo-kernel install kernel-lt
+yum --enablerepo=elrepo-kernel install -y kernel-lt
 #yum --enablerepo=elrepo-kernel install kernel-ml
 #reboot
 yum -y install yum-utils net-tools device-mapper-persistent-data lvm2
